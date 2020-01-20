269214514082178ffc877df3e3fb158898c7e3dd
at-vuongtran <vuong.tran@asiantech.vn>
2020-01-20 09:55:20 +0700

    finnish chapter04 learn ruby in rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Return full title for the per-page basic	
+	def full_tittle(page_tittle = "")
+		base_tittle = "Ruby on Rails Tutorial Sample App"
+		if page_tittle.empty?
+			base_tittle
+		else
+			"#{page_tittle} | #{base_tittle}"
+		end
+	end
 end
