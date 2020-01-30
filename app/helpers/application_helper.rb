3a6d63c9871b8e509e394f78867fa1dc7da384ae
at-vuongtran <vuong.tran@asiantech.vn>
2020-01-30 17:46:19 +0700

    Make a basic User model (including secure passwords)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def full_tittle page_tittle = ""
-		base_tittle = "Ruby on Rails Tutorial Sample App"
-		page_tittle.blank? ? base_tittle : "#{page_tittle} | #{base_tittle}"
-	end
+  def full_tittle page_tittle = ""
+    base_tittle = "Ruby on Rails Tutorial Sample App"
+    page_tittle.blank? ? base_tittle : "#{page_tittle} | #{base_tittle}"
+  end
 end
